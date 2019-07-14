de85b701b7089b9b204f42d68eccb118c19b478f
Rishabh Rao <rishabhsrao@gmail.com>
2019-07-14 20:48:09 +0200

    :wrench: Fix Jest config import issues

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,3 @@
 // See https://jestjs.io/docs/en/configuration.html
 
-module.exports = {
-  extends: './node_modules/@magica11y/cauldron/jest.config.js',
-};
+module.exports = require('./node_modules/@magica11y/cauldron/jest.config.js');
